0173b403b005f570f089ba7e4b629b7415018fc2
Wesley Luiz <wesleylm03@outlook.com>
2025-04-01 15:45:46 -0300

    Atividade04

---
+++ atividade04.py
@@ -0,0 +1,8 @@
+number1 = int(input("Digite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
+
+if number1 > number2:
+    print(number2, number1)
+else:
+    print(number1, number2)
+    
