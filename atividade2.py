f61e794726d698391c541352f90577909af17f49
Wesley Luiz <wesleylm03@outlook.com>
2025-04-01 14:42:00 -0300

    Atividade03

--- atividade2.py
+++ atividade2.py
@@ -1,1 +1,4 @@
-nome =
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+print(f"A soma de {n1} e {n2} é {n1 + n2}\nA Subtração de {n1} e {n2} é {n1 - n2}\nA multiplicação de"
+      f" {n1} e {n2} é {n1 * n2}\nA divisão de {n1} e {n2} é {n1 / n2}.")
