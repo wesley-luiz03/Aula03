6f4cf6fa0767e17fa7e2729736c656e09bd7b4f7
Wesley Luiz <wesleylm03@outlook.com>
2025-04-01 14:26:11 -0300

    Atividade02

--- atividade1.py
+++ atividade1.py
@@ -1,8 +1,13 @@
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-salario = float(input("Digite seu salário: "))
+while True:
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    salario = float(input("Digite seu salário: "))
+    reajuste = int(input("Digite o percentual de reajuste: "))
 
-if idade >= 18:
-    print(f"Olá! {nome}, você tem {idade} (maior de idade) anos e recebe {salario}!")
-else:
-    print(f"Olá! {nome}, você tem {idade} (menor de idade) anos e recebe {salario}!")
+    novo_salario = ((reajuste * salario / 100) + salario)
+
+    if idade >= 18:
+        print(f"Olá! {nome}, você tem {idade} anos e seu novo salário é {novo_salario:.2f}!")
+    else:
+        print("Sinto muito! Você não pode responder por ser menor de idade!")
+        continue
